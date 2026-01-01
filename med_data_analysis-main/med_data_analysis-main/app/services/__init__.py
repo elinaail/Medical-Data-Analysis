a6ed6ed6fcb764a817a5001b3bb656e39eeaccaf
Aleksandra Savenkova <adsavenkova@edu.hse.ru>
2026-01-01 19:20:39 +0300

    Remove med_data_analysis-main

--- med_data_analysis-main/med_data_analysis-main/app/services/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.services.prediction import prediction_service, PredictionService
-from app.services.history import HistoryService
-
-__all__ = ["prediction_service", "PredictionService", "HistoryService"]
